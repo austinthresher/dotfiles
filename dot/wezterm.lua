ac90be41405d76b75255d6ffc79081b1f4f70fd5
Austin Thresher <austin@dangermomentum.com>
2022-10-18 12:37:38 -0600

    Cursor tweaks

--- dot/wezterm.lua
+++ dot/wezterm.lua
@@ -1,11 +1,19 @@
 local wezterm = require 'wezterm'
-return {
-    --color_scheme = "midnight-in-mojave",
-    --color_scheme = "Dark+",
-    color_scheme = "Classic Dark (base16)",
-    -- color_scheme = "ayu",
 
-    window_background_opacity = 0.99,
+--color_scheme = "midnight-in-mojave",
+-- color_scheme = "ayu",
+--color_scheme = "Dark+",
+local scheme = wezterm.get_builtin_color_schemes()['Classic Dark (base16)']
+scheme.cursor_fg = "#000000"
+scheme.cursor_bg = "#DDDDDD"
+
+return {
+    color_schemes = {
+        ['Custom'] = scheme
+    },
+    color_scheme = "Custom",
+
+    window_background_opacity = 0.98,
     font_size = 13,
     font = wezterm.font('JetBrains Mono', {weight="Light"}),
     font_rules = {
@@ -20,8 +28,9 @@ return {
     bold_brightens_ansi_colors = false,
     window_padding = { left=0, right=0, top=0, bottom=0 },
     animation_fps = 60,
-    default_cursor_style = "BlinkingBlock",
-    cursor_blink_rate = 800,
+    default_cursor_style = "BlinkingUnderline",
+    cursor_thickness = 2,
+    cursor_blink_rate = 400,
     cursor_blink_ease_in = "Linear",
     cursor_blink_ease_out = "EaseOut",
 }
