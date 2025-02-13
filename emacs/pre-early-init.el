926c79b54b12b5189d40984932fd3d203783380c
Austin Thresher <austin@dangermomentum.com>
2025-02-13 15:45:54 -0700

    swap from kwin to picom

--- emacs/pre-early-init.el
+++ emacs/pre-early-init.el
@@ -1,6 +1,8 @@
 ;;; -*- no-byte-compile: t; lexical-binding: t; -*-
 (setq minimal-emacs-ui-features '(context-menu tooltips dialogs))
 (setq use-package-enable-imenu-support t)
+(push '(background-color . "#000") default-frame-alist)
+(push '(foreground-color . "#CCC") default-frame-alist)
 (push '(width . 190) default-frame-alist)
 (push '(height . 50) default-frame-alist)
 (push '(tab-bar-lines . 1) default-frame-alist)
